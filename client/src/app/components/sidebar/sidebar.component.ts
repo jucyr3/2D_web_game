a5bec14c66a0667d4707013112a2b628d4adc018
VincentCharbonneau <vincent-2.charbonneau@polymtl.ca>
2025-01-20 14:11:10 -0500

    feat(commonFiles) add and edit common files

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
-}
+export class SidebarComponent {}
---
+++ common/map.ts
@@ -0,0 +1,27 @@
+import { Tile } from "./tile";
+import { TILE_TYPES } from "./tileType.constants";
+
+export class Map {
+    name: string;
+    id:number;
+    readonly size: number;
+    isVisible: boolean;
+    description: string;
+    gameMode: "CTF" | "Classic";
+    tileMatrix: Tile[][];
+    lastModified: Date;
+
+    constructor(name: string, size: number, isVisible: boolean, description: string, gameMode: "CTF" | "Classic") {
+        this.name = name;
+        this.size = size;
+        this.isVisible = isVisible;
+        this.description = description;
+        this.gameMode = gameMode;
+        this.tileMatrix = Array.from({ length: size }, () => Array.from({ length: size }, () => new Tile(TILE_TYPES.GROUND_1, false, false)));
+        this.lastModified = new Date();
+    }
+
+    get flattenedTileMatrix(): Tile[] {
+        return this.tileMatrix.reduce((acc, row) => [...acc, ...row], []);
+      }
+}
