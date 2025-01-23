829d05fdc020365f8b265a56534c0cc4214c4f2f
oolong22 <tristansamson21@hotmail.com>
2025-01-23 12:14:25 -0500

    feature(map): Added loading tile textures/items from map

--- client/src/app/components/tile/tile.component.ts
+++ client/src/app/components/tile/tile.component.ts
@@ -43,6 +43,10 @@ export class TileComponent implements OnInit {
     ngOnInit() {
         this.tileTexture = this.editingToolService.getTileImage(this.tileObject.type); // Initialize here
         this.tileType = this.tileObject.type;
+        if (this.tileObject.gameObject) {
+            this.itemObject = this.tileObject.gameObject;
+            this.itemService.decreaseItemAmount(this.itemObject.name);
+        }
 
         const row = Math.floor(this.tileNumber / this.mapService.map.size);
         const column = this.tileNumber % this.mapService.map.size;
