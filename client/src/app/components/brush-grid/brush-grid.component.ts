0dba23997ea6dc135e0abe29a045ee191962c363
Vincent Charbonneau <vincent-2.charbonneau@polymtl.ca>
2025-01-25 10:13:10 -0500

    feat(brush) add slected brush highlight

--- client/src/app/components/brush-grid/brush-grid.component.ts
+++ client/src/app/components/brush-grid/brush-grid.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { EditToolTypes } from '@app/services/editing-tool.constants';
 import { EditingToolService } from '@app/services/editing-tool.service';
 import { TileTypes } from '@common/tileType.constants';
+import { BrushComponent } from "../brush/brush.component";
 
 @Component({
     selector: 'app-brush-grid',
-    imports: [],
+    imports: [BrushComponent],
     templateUrl: './brush-grid.component.html',
     styleUrl: './brush-grid.component.scss',
 })
 export class BrushGridComponent {
-    editToolTypes = EditToolTypes; // ? dw about it, its needed
     tileTypes = TileTypes; // ? dw about it, its needed
 
-    constructor(private readonly editingToolService: EditingToolService) {}
+    constructor(protected readonly editingToolService: EditingToolService) {}
 
     changeTool(tool: EditToolTypes) {
         this.editingToolService.setActiveTool(tool);
