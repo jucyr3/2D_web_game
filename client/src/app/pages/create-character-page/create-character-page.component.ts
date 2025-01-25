6b8370fd1da2438f2da2ae8addc73f1078d98656
NeonGhost314 <144621932+NeonGhost314@users.noreply.github.com>
2025-01-25 18:04:35 -0500

    feat(warningMessage): Add warning alert for creating character

--- client/src/app/pages/create-character-page/create-character-page.component.ts
+++ client/src/app/pages/create-character-page/create-character-page.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { ProfileSelectionComponent } from '@app/components/create-character/profile-selection/profile-selection.component';
 import { ProfileShowcaseComponent } from '@app/components/create-character/profile-showcase/profile-showcase.component';
 import { ProfileService } from '@app/services/profile.service';
 
 @Component({
     selector: 'app-create-character-page',
-    imports: [ProfileSelectionComponent, ProfileShowcaseComponent, RouterLink],
+    imports: [ProfileSelectionComponent, ProfileShowcaseComponent],
     styleUrl: './create-character-page.component.scss',
     templateUrl: './create-character-page.component.html',
 })
 export class CreateCharacterPageComponent {
+    constructor(
+        private profileService: ProfileService,
+        public router: Router,
+    ) {}
     verifyCreation() {
         if (this.profileService.getName()) {
             this.router.navigate(['/waitingRoom']);
+        } else {
+            alert('Veuillez Choisir le nom de votre personnage');
         }
     }
-
-    constructor(
-        private profileService: ProfileService,
-        private router: Router,
-    ) {}
 }
