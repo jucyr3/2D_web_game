11c59c67c3579bb9bf82d033f8ddbc5813f498f6
Oolong22 <tristansamson21@hotmail.com>
2025-02-03 15:08:51 -0500

    refactor(map): convert Map class to interface and add MapVerification interface

--- common/map.ts
+++ common/map.ts
@@ -1,33 +1,12 @@
 import { Tile } from './tile';
-import { TileTypes } from './tileType.constants';
 
-export class Map {
-    name: string;
+export interface Map {
     id: number;
+    name: string;
     readonly size: number;
     isVisible: boolean;
     description: string;
     gameMode: 'CTF' | 'Classic';
     tileMatrix: Tile[][];
     lastModified: Date;
-
-    constructor(name: string, id:number, size: number, isVisible: boolean, description: string, gameMode: 'CTF' | 'Classic', tileMatrix?: Tile[][]) {
-        this.name = name;
-        this.id = id;
-        this.size = size;
-        this.isVisible = isVisible;
-        this.description = description;
-        this.gameMode = gameMode;
-        if (tileMatrix) {
-            this.tileMatrix = tileMatrix;
-        } else {
-            this.tileMatrix = Array.from({ length: size }, () => Array.from({ length: size }, () => new Tile(TileTypes.GROUND_1, false, false)));
-        }
-
-        this.lastModified = new Date();
-    }
-
-    get flattenedTileMatrix(): Tile[] {
-        return this.tileMatrix.reduce((acc, row) => [...acc, ...row], []);
-    }
 }
