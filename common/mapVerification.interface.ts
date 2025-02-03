7541ff6e5d33b50e1dca306660f0b6b35cb733c6
danyali84 <lixdanya84@gmail.com>
2025-02-03 10:39:14 -0500

    feat(saveButton):  added an Object that sends the error instead of a list

---
+++ common/mapVerification.interface.ts
@@ -0,0 +1,14 @@
+
+
+export interface mapVerification {
+    isUniqueName: boolean;
+    isNamePresent: boolean;
+    isDescriptionPresent: boolean;
+    isMapHalfFloor: boolean;
+    isMapAccessible: boolean;
+    areStartingPointsValid: boolean;
+    areDoorsNextToWalls: boolean;
+    areDoorsNotNextToBorder: boolean;
+
+
+}
