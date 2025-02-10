d0d5d0c81f18c79ac27124f5b9ec49e05ed2fa43
VincentCharbonneau <vincent-2.charbonneau@polymtl.ca>
2025-02-10 18:15:01 -0500

    feature(loadingModal): add loading modal component and service
    Co-authored-by: Oolong22 <Oolong22@users.noreply.github.com>

---
+++ client/src/app/components/loading-modal/loading-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-loading-modal',
+    templateUrl: './loading-modal.component.html',
+    styleUrl: './loading-modal.component.scss',
+})
+export class LoadingModalComponent {}
